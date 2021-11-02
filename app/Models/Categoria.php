9ae8fed4397ff5b20816205c48a9fe51d37c79aa
nunez-moreno-joseantonio <nunez.moreno.joseantonio@gmail.com>
2021-11-02 10:02:22 +0100

    cambios modelos

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,19 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Categoria extends Model
 {
     use HasFactory;
 
-    use SoftDeletes;
-
     protected $primaryKey = 'categoria_id';
 
-    public function Cursos()
+    public function Tareas()
     {
-        return $this->belongsToMany(Curso::class);
+        return $this->belongsToMany(Tarea::class);
     }
     
 }
